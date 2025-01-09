d2ac4b3f7adbe2ef8d0de8a635392c82478d14c1
Vasya <oiuy78@yandex.ru>
2025-01-09 03:34:07 +0300

    Add close session

--- src/entrypoint/scripts/db_dump.py
+++ src/entrypoint/scripts/db_dump.py
@@ -18,6 +18,7 @@ async def send_dump_file() -> None:
     file = FSInputFile(config.app.DB_DUMP_FILE_PATH)
     for owner_chat_id in config.bot.owners_chat_id:
         await bot.send_document(owner_chat_id, file)
+    await bot.session.close()
 
 
 if __name__ == "__main__":
