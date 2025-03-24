9169ab4e1eab59a4c2d0d6665d28328f86f3f45c
Vasya <oiuy78@yandex.ru>
2025-03-25 00:16:06 +0300

    Add history table

--- src/infrastructure/migrations/base.py
+++ src/infrastructure/migrations/base.py
@@ -3,3 +3,4 @@ from src.infrastructure.models.order import Order, Bid
 from src.infrastructure.models.star import Star
 from src.infrastructure.models.transaction import Lt, WithdrawRequest
 from src.infrastructure.models.user import User, UserReferral
+from src.infrastructure.models.history import History
