a9d7372375cd9295f37ce689abe57669c5bf22af
Vasya <oiuy78@yandex.ru>
2025-04-07 19:15:16 +0300

    Fix get many giveaways

--- src/infrastructure/gateways/giveaway.py
+++ src/infrastructure/gateways/giveaway.py
@@ -30,9 +30,11 @@ class GiveawayGateway(GiveawaySaver, GiveawayReader):
             return GiveawayDM(**giveaway.__dict__)
 
     async def get_many(self, type: str, user_id: int) -> list[GiveawayDM]:
-        conditions = [Giveaway.is_completed == False, Giveaway.end_time > datetime.now()]
+        conditions = [Giveaway.end_time > datetime.now()]
         if type == "user":
-            conditions.append(or_(Giveaway.user_id == user_id, Giveaway.participants_ids.contains([user_id])))
+            conditions.append(
+                or_(Giveaway.user_id == user_id, Giveaway.participants_ids.contains([user_id]))
+            )
 
         stmt = select(Giveaway).where(*conditions).order_by(Giveaway.end_time)
         result = await self._session.execute(stmt)
