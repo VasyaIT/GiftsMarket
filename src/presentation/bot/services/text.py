50904d64204d1609a083d69ac943b32fdba0d0e9
Vasya <oiuy78@yandex.ru>
2025-04-16 15:04:46 +0300

    Fix text

--- src/presentation/bot/services/text.py
+++ src/presentation/bot/services/text.py
@@ -182,7 +182,7 @@ def get_ended_giveaway_text(
     for gift in gifts:
         link = f"https://t.me/nft/{gift.type.replace(' ', '').replace('-', '')}-{gift.number}"
         gifts_links.append(f"<a href='{link}'>{gift.type} #{gift.number}</a>")
-    gifts_text = f"Gifts ({len(gifts)}):\n\n{'\n\n'.join(gifts_links)}"
+    gifts_text = f"Gifts ({len(gifts)}):\n\n{', '.join(gifts_links)}"
     text = f"""
 🎊 <b>Giveaway Ended!</b> 🎊
 
